d8baf4ae76a112642c4d4dde17250b8dec261359
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-03-31 20:52:18 +0300

    added class component

---
+++ social-network/src/components/Users/UsersС.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import styles from "./users.module.css";
+import userPhoto from "../../avatars/images2.png";
+import {InitialUsersStateType} from "./UsersContainer";
+import axios from "axios";
+
+
+export class UsersC extends React.Component<InitialUsersStateType> {
+
+    getUsers = () => {
+        if (this.props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => {
+                    this.props.setUsers(response.data.items)
+                })
+        }
+    }
+
+    render = () => {
+        return <div>
+            <button onClick={this.getUsers}>Дай стэйт сука!</button>
+            {
+                this.props.users.map(user => <div key={user.id} className={styles.all}>
+                <span>
+                    <div>
+                        <img src={user.photos.small ? user.photos.small : userPhoto} alt="avatar"
+                             className={styles.userAvatar}/>
+                    </div>
+                    <div>
+                        {user.followed
+                            ? <button onClick={() => {
+                                this.props.unfollow(user.id)
+                            }}>Unfollow</button>
+                            : <button onClick={() => {
+                                this.props.follow(user.id)
+                            }}>Follow</button>}
+                    </div>
+                </span>
+                    <span>
+                    <span>
+                        <div>{user.name}</div>
+                        <div>{user.status}</div>
+                    </span>
+                    <span>
+                        <div>{"user.location.country"}</div>
+                        <div>{"user.location.city"}</div>
+                    </span>
+                </span>
+                </div>)
+            }
+        </div>
+    }
+}
+
