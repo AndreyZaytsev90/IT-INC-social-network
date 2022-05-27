540427fafcdef9f2d46277e23b3f36ae9c2e04b5
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-05-27 22:25:45 +0300

    Added new files

---
+++ social-network/src/components/common/Preloader/Preloader.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import preloader from "../../../avatars/preloader.svg";
+
+export const Preloader = () => {
+    return (
+        <div><img alt={preloader} src={preloader}/></div>
+    );
+};
+
